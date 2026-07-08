6bac99bdeaae3965cafdd12dd2eb9d385c35d4e2
masterdimplee-ops <masterdimplee@gmail.com>
2026-07-09 00:55:28 +0800

    Refactor main.js for improved readability and structure

--- main.js
+++ main.js
@@ -1,81 +1,150 @@
 const config = {
     type: Phaser.AUTO,
+
     width: 900,
     height: 500,
-    backgroundColor: "#87CEEB",
+
+    parent: "game",
+
     physics: {
         default: "arcade",
         arcade: {
-            gravity: { y: 800 },
+            gravity: {
+                y: 800
+            },
             debug: false
         }
     },
+
     scene: {
-        preload,
-        create,
-        update
+        preload: preload,
+        create: create,
+        update: update
     }
 };
 
+
 const game = new Phaser.Game(config);
+
 
 let player;
 let platforms;
 let cursors;
 
+
+// Load aset
 function preload() {
 
-    // Membuat tekstur sederhana tanpa gambar
-    this.add.graphics()
-        .fillStyle(0x00aa00)
-        .fillRect(0,0,80,20)
-        .generateTexture("ground",80,20);
-
-    this.add.graphics()
-        .fillStyle(0xff4444)
-        .fillRect(0,0,40,50)
-        .generateTexture("player",40,50);
+    // karakter sementara (kotak)
+    this.load.image(
+        "player",
+        "https://labs.phaser.io/assets/sprites/phaser-dude.png"
+    );
 
 }
 
+
+// Membuat dunia game
 function create() {
 
+
+    // membuat platform
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(450,490,"ground").setScale(12,1).refreshBody();
 
-    platforms.create(300,380,"ground");
+    platforms.create(
+        450,
+        480,
+        null
+    )
+    .setDisplaySize(900,40)
+    .refreshBody();
 
-    platforms.create(600,300,"ground");
 
-    platforms.create(760,220,"ground");
 
-    player = this.physics.add.sprite(100,300,"player");
+    platforms.create(
+        300,
+        350,
+        null
+    )
+    .setDisplaySize(200,20)
+    .refreshBody();
 
-    player.setBounce(0.1);
+
+
+    platforms.create(
+        650,
+        250,
+        null
+    )
+    .setDisplaySize(200,20)
+    .refreshBody();
+
+
+
+    // player
+    player = this.physics.add.sprite(
+        100,
+        300,
+        "player"
+    );
+
+
+    player.setScale(0.8);
+
+    player.setBounce(0.2);
 
     player.setCollideWorldBounds(true);
 
-    this.physics.add.collider(player, platforms);
 
+
+    // collision player dengan platform
+    this.physics.add.collider(
+        player,
+        platforms
+    );
+
+
+
+    // keyboard
     cursors = this.input.keyboard.createCursorKeys();
 
 }
 
+
+// Update setiap frame
 function update() {
 
+
     if(cursors.left.isDown){
-        player.setVelocityX(-220);
-    }
-    else if(cursors.right.isDown){
-        player.setVelocityX(220);
-    }
-    else{
-        player.setVelocityX(0);
+
+        player.setVelocityX(-250);
+
     }
 
-    if(cursors.up.isDown && player.body.touching.down){
-        player.setVelocityY(-500);
+    else if(cursors.right.isDown){
+
+        player.setVelocityX(250);
+
+    }
+
+    else{
+
+        player.setVelocityX(0);
+
+    }
+
+
+
+    // lompat
+
+    if(
+        cursors.up.isDown &&
+        player.body.touching.down
+    ){
+
+        player.setVelocityY(-450);
+
     }
 
 }
