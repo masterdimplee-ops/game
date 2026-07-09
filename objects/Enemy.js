ff986dc0ea6f60bdbe6df20c487fec8ac11857f8
masterdimplee-ops <masterdimplee@gmail.com>
2026-07-09 10:49:22 +0800

    Refactor Enemy class to extend Phaser.Sprite

--- objects/Enemy.js
+++ objects/Enemy.js
@@ -1,3 +1,15 @@
-class Enemy{
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y) {
+
+        super(scene, x, y, "enemy");
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.setImmovable(true);
+        this.body.allowGravity = false;
+
+    }
 
 }
