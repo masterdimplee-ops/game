62b4d61972256dc4313351cf2ce169bc5415dc90
masterdimplee-ops <masterdimplee@gmail.com>
2026-07-09 10:51:08 +0800

    Refactor Coin class to extend Phaser.Sprite

--- objects/Coin.js
+++ objects/Coin.js
@@ -1,3 +1,18 @@
-class Coin{
+class Coin extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y) {
+
+        super(scene, x, y, "coin");
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.body.allowGravity = false;
+
+    }
+
+    collect() {
+        this.destroy();
+    }
 
 }
