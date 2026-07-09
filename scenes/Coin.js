f6318e52bec915eb52d8283a370f12286b0eb1a9
masterdimplee-ops <masterdimplee@gmail.com>
2026-07-09 10:45:47 +0800

    Add Coin class for coin collection in Phaser

---
+++ scenes/Coin.js
@@ -0,0 +1,18 @@
+class Coin extends Phaser.Physics.Arcade.Sprite {
+
+    constructor(scene, x, y) {
+
+        super(scene, x, y, "coin");
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.body.allowGravity = false;
+
+    }
+
+    collect() {
+        this.destroy();
+    }
+
+}
